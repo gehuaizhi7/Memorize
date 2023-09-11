74ba14e18de58777c99643d2d32be16b2a869134
Huaizhi Ge <gehuaizhi@outlook.com>
2023-09-11 16:01:55 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Huaizhi Ge on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
